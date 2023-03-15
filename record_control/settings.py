a46a9a52dba0328894b7519dddf1015fef60e7c5
alex23090 <alex2003199604@gmail.com>
2023-03-15 20:35:01 +0200

    Added Sqlite in case if it's needed

--- record_control/settings.py
+++ record_control/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'record_control.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+# POSTGRESQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -114,6 +114,13 @@ DATABASES = {
     }
 }
 
+# SQLITE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
